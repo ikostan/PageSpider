7e8da5fb4fdcd1f165085dccc8966a8675473619
ikostan <igorkostan@gmail.com>
2017-12-27 21:41:17 -0800

    Comments for methods added

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -2,10 +2,19 @@
 
 
 def create_database(database_path: str):
+    """
+    Generates the database
+    :type database_path: object str
+    """
     # TODO: generate the database
     pass
 
 
 def save_words_to_database(database_path: str, words_list: list):
+    """
+    Save the word to DB
+    :type database_path: object str
+    :type words_list: object list
+    """
     # TODO: save the word to DB
     pass
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -2,15 +2,27 @@
 
 
 def load_urls_from_file(file_path: str):
+    """
+    Read the text file with the URL in it
+    :type file_path: object str
+    """
     # TODO: add the code needed to read the text file with the URL in it
     pass
 
 
 def load_page(url: str):
+    """
+    Reads the URL
+    :type url: object str
+    """
     # TODO: add the code needed that reads the URL
     pass
 
 
-def scrape_page(oage_content: str):
-    # TODO: analyze the text
+def scrape_page(page_content: str):
+    """
+    Analyzes the text
+    :type page_content: object str
+    """
+    # TODO: analyzes the text
     pass
