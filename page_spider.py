bb8ab50d1e812f20f5d826ec4337f3914993cd23
ikostan <igorkostan@gmail.com>
2017-12-27 22:29:29 -0800

    page_spider.py: main method > open file, read urls and scrap html content

--- page_spider.py
+++ page_spider.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-
 import os
 import argparse
+from utilities import url_utilities
 
 
 def main(database: str, url_list_file: str):
     print('Python Spider:\n')
     print('We are going to work with "{}" database and we going to scan "{}"'
           .format(database, url_list_file))
+
+    big_word_list = []
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print('reading '.format(url))
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_content)
+        big_word_list.extend(words)
 
 
 if __name__ == "__main__":
