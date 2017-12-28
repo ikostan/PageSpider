59995044c10b336b2aa420e72dd33976cd9b9f43
ikostan <igorkostan@gmail.com>
2017-12-27 21:58:04 -0800

    url_uyilities.py: methods implementation: load_urls_from_file, load_page

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from urllib.request import urlopen
 
 
 def load_urls_from_file(file_path: str):
@@ -6,8 +7,16 @@ def load_urls_from_file(file_path: str):
     Read the text file with the URL in it
     :type file_path: object str
     """
-    # TODO: add the code needed to read the text file with the URL in it
-    pass
+    try:
+        with open(file_path) as file:
+            content = file.readline()
+            return content
+    except FileNotFoundError as e:
+        print('{}: {}'.format(e.strerror, e.filename))
+        exit(2)
+    except Exception as e:
+        print(e)
+        exit(2)
 
 
 def load_page(url: str):
@@ -15,8 +24,9 @@ def load_page(url: str):
     Reads the URL
     :type url: object str
     """
-    # TODO: add the code needed that reads the URL
-    pass
+    response = urlopen(url)
+    html = response.read.decode('utf-8')
+    return html
 
 
 def scrape_page(page_content: str):
