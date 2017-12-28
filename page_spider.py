f8983880d8df9dea8218b91100ae272acf8ad6b2
ikostan <igorkostan@gmail.com>
2017-12-28 13:32:39 -0800

    words.db + words table

--- page_spider.py
+++ page_spider.py
@@ -2,12 +2,15 @@
 import os
 import argparse
 from utilities import url_utilities
+from utilities import database_utilities
 
 
 def main(database: str, url_list_file: str):
     print('Python Spider:\n')
     print('We are going to work with "{}" database and we going to scan "{}"'
           .format(database, url_list_file))
+
+    database_utilities.create_database(database)
 
     big_word_list = []
     urls = url_utilities.load_urls_from_file(url_list_file)
@@ -25,4 +28,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
+
     main(database=database_file, url_list_file=input_file)
--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+import sqlite3
+import os
+
+
+def create_words_table(db):
+    sql = "CREATE TABLE 'words' ('word' TEXT NOT NULL UNIQUE,\
+                                'usage_count' INTEGER NOT NULL DEFAULT 1,\
+                                PRIMARY KEY('word'));"
+    db.execute(sql)
+    db.commit()
 
 
 def create_database(database_path: str):
@@ -6,8 +16,16 @@ def create_database(database_path: str):
     Generates the database
     :type database_path: object str
     """
-    # TODO: generate the database
-    pass
+    try:
+        if not os.path.exists(database_path):
+            db = sqlite3.connect(database_path)
+            create_words_table(db)
+            print('A new database is created: {}'.format(database_path))
+        print('SQLite version: {}'.format(sqlite3.version))
+    except Exception as e:
+        print(e)
+    finally:
+        db.close()
 
 
 def save_words_to_database(database_path: str, words_list: list):
